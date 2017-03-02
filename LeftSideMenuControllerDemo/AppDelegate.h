70d5c2b9bb59a1cd14672ec14c02efbb56585997
tomfriwel <976932447@qq.com>
2017-03-02 15:29:01 +0800

    Initial Commit

---
+++ LeftSideMenuControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeftSideMenuControllerDemo
+//
+//  Created by tomfriwel on 02/03/2017.
+//  Copyright © 2017 tomfriwel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LeftSideMenuControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LeftSideMenuControllerDemo
+//
+//  Created by tomfriwel on 02/03/2017.
+//  Copyright © 2017 tomfriwel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
